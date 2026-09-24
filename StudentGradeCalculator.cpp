8d52b4cc80e8b3f36d3e8de2b73a0d87eaccf846
Azyz Ben Soussia <soussiaaziz10@gmail.com>
2026-09-24 07:45:24 +0100

    Add configuration files for C++ development and update main application logic

--- StudentGradeCalculator.cpp
+++ StudentGradeCalculator.cpp
@@ -9,14 +9,19 @@ double calculateAverage(int grade1, int grade2, int grade3)
 
 int main()
 {
-    string studentName = "Ahmad";
+    string studentName = "Azizos";
     int grade1 = 85;
     int grade2 = 90;
     int grade3 = 80;
+    int maxNumOfSubjects = 3;
+
+
+
 
     double average = calculateAverage(grade1, grade2, grade3);
-
+    cout << "Welcome To The App Average Calculation : " << endl;
     cout << "Student: " << studentName << endl;
+    cout << "Max Number of Subjects: " << maxNumOfSubjects << endl;
     cout << "Grade 1: " << grade1 << endl;
     cout << "Grade 2: " << grade2 << endl;
     cout << "Grade 3: " << grade3 << endl;
